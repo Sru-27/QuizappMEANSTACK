07fe19e627f4180098749f0e7766796f5f9d7c7d
sruthi murali <muralishruthi2959@gmail.com>
2022-08-05 16:53:13 +0530

    Init commit

---
+++ quiz/src/app/student/student.guard.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  doSomthing() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userid');
+    localStorage.removeItem('student');
+    localStorage.removeItem('teacher');
+    this.router.navigate(['/'])
+  }
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn())
+    {
+      if (localStorage.getItem('student') == "yes")
+      {
+          if (localStorage.getItem('teacher') == "no")
+          {
+            return true;
+          }
+          else
+          {
+            this.doSomthing();
+            return false;
+          }
+        }
+        else
+        {
+          this.doSomthing();
+          return false;
+        }
+
+      }
+      else
+      {
+        this.doSomthing();
+        return false;
+      }
+    }
+  }
